afa18dc2e957ee615ccc3af3f617d741bb17ac9c
Eduardo Santiago <eduardosantiago_@hotmail.com>
2025-06-17 11:38:31 -0300

    feat: add customer request and hooks application

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,61 @@
+import { useEffect, useState, useRef } from "react";
+import type { FormEvent } from "react";
+import { FiTrash } from "react-icons/fi";
+import { api } from "./services/api";
+
+interface CustomerProps {
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
+
 export default function App() {
+  const [customers, setCustomers] = useState<CustomerProps[]>([]);
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    loadCustomers();
+  }, []);
+
+  async function loadCustomers() {
+    try {
+      const response = await api.get("/customers");
+      setCustomers(response.data);
+    } catch (error) {
+      console.error("Erro ao carregar clientes:", error);
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!nameRef.current || !emailRef.current) return;
+
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+    });
+
+    setCustomers((allCustomers) => [...allCustomers, response.data]);
+  }
+
   return (
     <div className="w-full min-h-screen  bg-gray-900 flex justify-center px-4">
-      <main className="my-10 w-full md:max-w-2x1">
-        <h1 className="text-4x1 font-medium text-white">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">
           Registro de Clientes
         </h1>
 
-        <form className="flex flex-col my-6">
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
           <label className="font-medium text-white">Nome</label>
           <input
             type="text"
             placeholder="Digite o nome do cliente..."
             className="w-full mb-5 p-2 rounded"
+            ref={nameRef}
           />
 
           <label className="font-medium text-white">Email</label>
@@ -19,6 +63,7 @@ export default function App() {
             type="text"
             placeholder="Digite o email do cliente..."
             className="w-full mb-5 p-2 rounded"
+            ref={emailRef}
           />
 
           <input
@@ -29,18 +74,28 @@ export default function App() {
         </form>
 
         {/* TABELA QUE VAI MOSTRAR OS CLIENTES */}
-
-        <article className="w-full bg-white rounded p-2">
-          <p>
-            <span className="font-medium">Nome:</span> Mateus
-          </p>
-          <p>
-            <span className="font-medium">Email:</span> mateus@teste.com
-          </p>
-          <p>
-            <span className="font-medium">Status:</span> ATIVO
-          </p>
-        </article>
+        <section className="flex flex-col gap-4">
+          {customers.map((customer) => (
+            <article
+              key={customer.id}
+              className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
+            >
+              <p>
+                <span className="font-medium">Nome:</span> {customer.name}
+              </p>
+              <p>
+                <span className="font-medium">Email:</span> {customer.email}
+              </p>
+              <p>
+                <span className="font-medium">Status:</span>{" "}
+                {customer.status ? "Ativo" : "Inativo"}
+              </p>
+              <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+                <FiTrash size={18} color="#FFF" />
+              </button>
+            </article>
+          ))}
+        </section>
       </main>
     </div>
   );
